3a5db76a609d88737855d640b4b105b1dce7824f
Miguel Roman <mrm_632@hotmail.com>
2022-09-28 18:28:19 -0500

    Fusion del avance de todos.
    Fusion del avance de todos.

--- app/src/main/java/com/angeljimenez/moviesapp/fragments/Movies.java
+++ app/src/main/java/com/angeljimenez/moviesapp/fragments/Movies.java
@@ -1,21 +1,66 @@
 package com.angeljimenez.moviesapp.fragments;
 
-import android.os.Bundle;
+        import android.app.Activity;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
+        import androidx.fragment.app.Fragment;
+        import androidx.fragment.app.FragmentTransaction;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.Button;
+        import android.widget.RadioButton;
 
-import com.angeljimenez.moviesapp.R;
+        import com.angeljimenez.moviesapp.ExploreActivity;
+        import com.angeljimenez.moviesapp.R;
+        import com.angeljimenez.moviesapp.databinding.FragmentMoviesBinding;
 
 
-public class Movies extends Fragment {
+public class Movies extends Fragment implements View.OnClickListener {
+    FragmentMoviesBinding binding;
+    public Movies(){
+
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_movies, container, false);
+
+
+        binding = FragmentMoviesBinding.inflate(inflater,container, false);
+        binding.rbOrdenar1.setOnClickListener(this);
+
+        binding.rbOrdenar1.setChecked(true);
+        mostrarOrdenar1();
+        return binding.getRoot();
+
     }
+
+    @Override
+    public void onClick(View view) {
+        mostrarOrdenar2();
+
+    }
+
+    private void mostrarOrdenar2() {
+        VistaPelis2Fragment VistaPelis2Fragment = new VistaPelis2Fragment();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.contenedorPelis, VistaPelis2Fragment);
+        transaction.commit();
+    }
+
+    private void mostrarOrdenar1() {
+        VistaPelisProfileFragment VistaPelisProfileFragment = new VistaPelisProfileFragment();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.contenedorPelis, VistaPelisProfileFragment);
+        transaction.commit();
+    }
+
+
+
+
 }
--- app/src/main/java/com/angeljimenez/moviesapp/fragments/Search.java
+++ app/src/main/java/com/angeljimenez/moviesapp/fragments/Search.java
@@ -1,44 +1,72 @@
 package com.angeljimenez.moviesapp.fragments;
 
-import android.os.Bundle;
+        import android.app.Activity;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
+        import androidx.fragment.app.Fragment;
+        import androidx.fragment.app.FragmentTransaction;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.Button;
+        import android.widget.RadioButton;
 
-import com.angeljimenez.moviesapp.R;
+        import com.angeljimenez.moviesapp.ExploreActivity;
+        import com.angeljimenez.moviesapp.R;
+        import com.angeljimenez.moviesapp.databinding.FragmentMoviesBinding;
+        import com.angeljimenez.moviesapp.databinding.FragmentSearchBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Search#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Search extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
+public class Search extends Fragment implements View.OnClickListener {
+    FragmentSearchBinding binding;
+    public Search(){
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        binding = FragmentSearchBinding.inflate(inflater,container, false);
+        binding.rbOrdenar1.setOnClickListener(this);
+
+        binding.rbOrdenar1.setChecked(true);
+        mostrarOrdenar1();
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        mostrarOrdenar2();
+
+    }
+
+    private void mostrarOrdenar2() {
+        VistaPelis2Fragment VistaPelis2Fragment = new VistaPelis2Fragment();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.contenedorPelis, VistaPelis2Fragment);
+        transaction.commit();
+    }
+
+    private void mostrarOrdenar1() {
+        VistaPelisProfileFragment VistaPelisProfileFragment = new VistaPelisProfileFragment();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.contenedorPelis, VistaPelisProfileFragment);
+        transaction.commit();
+    }
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public Search() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Search.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Search newInstance(String param1, String param2) {
         Search fragment = new Search();
         Bundle args = new Bundle();
@@ -56,11 +84,11 @@ public class Search extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    //@Override
+    //public View onCreateView(LayoutInflater inflater, ViewGroup container,
+     //                        Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        //return inflater.inflate(R.layout.fragment_search, container, false);
     }
-}
+
+
