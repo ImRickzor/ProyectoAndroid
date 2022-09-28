8c0e16836cdac0d6ed57362e559550f7a2e6e064
GuersonDEV <guerson044@gmail.com>
2022-09-28 04:57:50 -0500

    Update: se añadio los elementos del profile

--- app/src/main/java/com/angeljimenez/moviesapp/DashboardActivity.java
+++ app/src/main/java/com/angeljimenez/moviesapp/DashboardActivity.java
@@ -2,13 +2,27 @@ package com.angeljimenez.moviesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        Button btnDone = findViewById(R.id.btnDone);
+        btnDone.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        startActivity(new Intent(this, ExploreActivity.class));
+
     }
 }
